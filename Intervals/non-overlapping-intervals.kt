65c9caaca013b301984c90d1d14f5475b8941c74
Minh Pham <pdminh0410@gmail.com>
2025-01-15 16:44:22 -0800

    Intervals: Non-overlapping Intervals (Annother approach: Given a collection of intervals, find the maximum number of intervals that are non-overlapping.)

--- Intervals/non-overlapping-intervals.kt
+++ Intervals/non-overlapping-intervals.kt
@@ -19,4 +19,21 @@ class Solution {
 
         return res
     }
+
+    // Annother approach. Given a collection of intervals, 
+    // find the maximum number of intervals that are non-overlapping.
+    fun eraseOverlapIntervals(intervals: Array<IntArray>): Int {
+        intervals.sortWith { a, b -> a[1] - b[1] }
+
+        var count = 1
+        var prevEnd = intervals[0][1]
+        for (i in 1 until intervals.size) {
+            if (intervals[i][0] >= prevEnd) {
+                prevEnd = intervals[i][1]
+                count++
+            }
+        }
+
+        return intervals.size - count
+    }
 }
