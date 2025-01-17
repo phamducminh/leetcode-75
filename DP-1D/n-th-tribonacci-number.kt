def605e7fb6b48ccfde081b7f58ddebcdd069d59
Minh Pham <pdminh0410@gmail.com>
2025-01-16 21:25:51 -0800

    DP - 1D: N-th Tribonacci Number (non-recursive)

---
+++ DP-1D/n-th-tribonacci-number.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun tribonacci(n: Int): Int {
+        if (n == 0) return 0
+        if (n == 1 || n == 2) return 1
+
+        val array = IntArray(38)
+        array[0] = 0
+        array[1] = 1
+        array[2] = 1
+
+        for (i in 3 until 38) {
+            array[i] = array[i - 1] + array[i - 2] + array[i - 3]
+        }
+
+        return array[n]
+    }
+}
