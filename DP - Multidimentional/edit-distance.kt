e29bf6aa164f33d0b27993c3fd2de1f25440f16e
Minh Pham <pdminh0410@gmail.com>
2025-01-22 22:06:39 -0800

    DP - Multidimentional: Edit Distance

---
+++ DP - Multidimentional/edit-distance.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun minDistance(word1: String, word2: String): Int {
+        val m = word1.length
+        val n = word2.length
+
+        val dp = Array(m+1) { IntArray(n+1) }
+
+        for (i in 0 .. m) {
+            dp[i][0] = i
+        }
+
+        for (j in 0 .. n) {
+            dp[0][j] = j
+        }
+
+        for (i in 1 .. m) {
+            for (j in 1 .. n) {
+                dp[i][j] = minOf(
+                    dp[i-1][j] + 1,
+                    dp[i][j-1] + 1,
+                    if (word1[i-1] == word2[j-1]) dp[i-1][j-1] else dp[i-1][j-1] + 1
+                )
+            }
+        }
+
+        return dp[m][n]
+    }
+}
